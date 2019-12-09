c692e11fc8caa78e655c14326b5a26162b739434
Brandon Williams <brando5393@gmail.com>
2019-12-08 21:29:48 -0600

    added some elements to home page and added function blocks

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+function startGame(){
+    namePrompt();
+}
+
+function namePrompt() {
+        Swal.fire({
+            icon: 'info',
+            title: 'Your Name Please',
+            text: 'Hi there I am glad that you decided to play, can I please have a name for the scoreboard?',
+            input: 'text',
+            confirmButtonText: 'Submit',
+       })
+
+}
+
+function game(){
+    
+}
